0c628c3fe03def29548b8b3459818bac0c739dbb
soohyun <soohyun6879@naver.com>
2020-09-26 21:41:42 +0900

    week4 spring study

---
+++ week4/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); //회원저장
+    Optional<Member> findById(Long id); //id로 회원을 찾음
+    Optional<Member> findByName(String name); //이름으로 회원을 찾음
+    List<Member> findAll(); //지금까지 저장된 회원리스트 반환
+}
